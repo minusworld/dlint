fe9ac0f06a5eb124ce99c0d0ac94d1e8610f96b0
grayson <grayson@returntocorp.com>
2019-12-06 11:45:06 -0800

    Re-add logic fix for sqlalchemy-filter

--- dlint/linters/bad_sqlalchemy_filter.py
+++ dlint/linters/bad_sqlalchemy_filter.py
@@ -57,7 +57,7 @@ class BadSqlalchemyFilter(base.BaseLinter):
         if node.attr in self.sqlalchemy_filter_methods:
             attr_sequence = tree.module_path(node)
             filter_index = _index(attr_sequence, node.attr)
-            limit_index = min([_index(attr_sequence, m) for m in self.sqlalchemy_limit_methods])
+            limit_index = max([_index(attr_sequence, m) for m in self.sqlalchemy_limit_methods])
             if limit_index >= 0 and limit_index < filter_index:
                 from_self_index = _index(attr_sequence, self.sqlalchemy_from_self)
                 if not (limit_index < from_self_index and from_self_index < filter_index):
